b2c947b15639a1b1f099acb6c266a496a097bef9
Ksenia <ksenxiety@gmail.com>
2023-02-22 16:00:25 +0300

    characteristic zero module almost done (works incorrectly)

--- quentin/characteristic_zero.py
+++ quentin/characteristic_zero.py
@@ -9,7 +9,7 @@ C = variants_df.shape[0]
 lessons = list(variants_df.index.values)
 lessons_triplets = list(combinations(lessons, 3))
 
-print("TRIPLETS OF LESSONS TO BE CHECKED FOR HAVING AN INTERMEDIATE: ", list(combinations(lessons, 3)))
+print("POSSIBLE LESSONS COMBINATIONS: ", list(combinations(lessons, 3)))
 
 def trimmed_permutations(triplet_permutation): #type of triplet_permutation is a list of tuples
     return triplet_permutation[0:3]
@@ -24,13 +24,23 @@ def triplets_permutations(lessons_triplets): #TODO trim intermediats
 
 
 possible_intermediates = triplets_permutations(lessons_triplets)
-print(possible_intermediates)
+print("TRIPLETS OF LESSONS TO BE CHECKED FOR HAVING AN INTERMEDIATE: ", possible_intermediates)
 
 def common_variants(triplets, variants_df):
-    print(len(variants_df.columns))
-    
+    counter = 0
+    variants_amount = len(variants_df.columns)
+    print(variants_amount)
+    for el in triplets:
+        for i in range(0, 1, variants_amount):
+            lesson1 = variants_df.loc[el[0]].iat[i]
+            possible_zero = variants_df.loc[el[1]].iat[i]
+            lesson2 = variants_df.loc[el[2]].iat[i]
+            #print(lesson1, lesson2, possible_zero)
+            if (lesson1 == lesson2 & lesson1 != possible_zero):
+                triplets.remove(el)
+    return triplets
 
-common_variants(possible_intermediates, variants_df)
+print("CHARACTERISTIC ZEROS:", common_variants(possible_intermediates, variants_df))
 
 
 
