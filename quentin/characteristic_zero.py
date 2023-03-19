58f5c90f49c42a2a99656daa7ebb81df47c96373
Ksenia <ksenxiety@gmail.com>
2023-03-19 17:47:28 +0300

    characteristic_zero module now works correctly

--- quentin/characteristic_zero.py
+++ quentin/characteristic_zero.py
@@ -1,4 +1,5 @@
 import input_processing as inp
+import itertools
 from itertools import combinations, permutations
 import numpy as np
 
@@ -14,7 +15,7 @@ print("POSSIBLE LESSONS COMBINATIONS: ", list(combinations(lessons, 3)))
 def trimmed_permutations(triplet_permutation): #type of triplet_permutation is a list of tuples
     return triplet_permutation[0:3]
 
-def triplets_permutations(lessons_triplets): #TODO trim intermediats 
+def triplets_permutations(lessons_triplets): 
     triplets = []
     for i in lessons_triplets:
         p = trimmed_permutations(list(permutations(i)))
@@ -26,22 +27,54 @@ def triplets_permutations(lessons_triplets): #TODO trim intermediats
 possible_intermediates = triplets_permutations(lessons_triplets)
 print("TRIPLETS OF LESSONS TO BE CHECKED FOR HAVING AN INTERMEDIATE: ", possible_intermediates)
 
-def common_variants(triplets, variants_df):
+def to_dictionary(variants_df):
+    return variants_df.to_dict('index')
+
+def if_possible_zero(lesson1, lesson2, possible_zero): #dicts 
     counter = 0
-    variants_amount = len(variants_df.columns)
+    l1 = list(lesson1.values())
+    l2 = list(lesson2.values())
+    pz = list(possible_zero.values())
+    print(l1, "\n", l2, "\n", pz, "\n" )
+    for (el1, el2, el3_pz) in zip(l1, l2, pz):
+        if (el1 == el2 & el1 != el3_pz):
+            counter = counter + 1
+    if (counter > 0):
+        return False 
+    else:
+        return True
+
+def same_variant_position(lesson1, lesson2):
+    counter = 0
+    l1 = list(lesson1.values())
+    l2 = list(lesson2.values())
+    for (el1, el2) in zip(l1, l2):
+        if(el1 == el2):
+            counter = counter + 1
+    if (counter > 0):
+        return True
+    else:
+        return False 
+
+
+def common_variants(triplets, variants_df):
+    variants_amount = len(variants_df.columns) 
     print(variants_amount)
+    zeros = []
+    variants_dict = variants_df.to_dict('index')
     for el in triplets:
-        for i in range(0, 1, variants_amount):
-            lesson1 = variants_df.loc[el[0]].iat[i]
-            possible_zero = variants_df.loc[el[1]].iat[i]
-            lesson2 = variants_df.loc[el[2]].iat[i]
-            #print(lesson1, lesson2, possible_zero)
-            if (lesson1 == lesson2 & lesson1 != possible_zero):
-                triplets.remove(el)
-    return triplets
+        lesson1 = str(el[0])
+        lesson2 = str(el[2])
+        possible_zero = str(el[1])
+        l1 = variants_dict.get(lesson1)
+        l2 = variants_dict.get(lesson2)
+        p_z = variants_dict.get(possible_zero)
+        print(el)
+        if (same_variant_position(l1, l2) == True & if_possible_zero(l1, l2, p_z) == True):
+                zeros.append(el)
+    return zeros
 
 print("CHARACTERISTIC ZEROS:", common_variants(possible_intermediates, variants_df))
-
 
 
 
